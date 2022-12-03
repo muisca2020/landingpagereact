a50760fb4bc742729cbb796986eca75665a7d24e
Leonardo Sanchez <leonsanc@gmail.com>
2022-12-03 11:45:34 -0600

    Adición de campo de texto y botón con comportamiento

--- todo-app/src/components/FormTodo.jsx
+++ todo-app/src/components/FormTodo.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FormTodo = () => {
-     return (
-        <div>
-            Form Todo
-        </div>
+const FormTodo = props => {
+    const [description, setDescription] = useState("");
+
+    return (
+        <form>
+            <div className="todo-list">
+                <div className="input">
+                    <input 
+                        type="text" 
+                        className="text"
+                        value={description}
+                        onChange={
+                            e => setDescription( e.target.value )
+                        } 
+                    />
+                    <button
+                        className="button-blue"
+                        disabled={description ? "": "disabled"}
+                    >
+                        Agregar tarea
+                    </button>
+                </div>
+            </div>
+        </form>
     );
 }
 
