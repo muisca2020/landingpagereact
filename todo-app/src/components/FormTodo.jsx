ce840ab782d77226caac044799722a3773034dc0
Leonardo Sanchez <leonsanc@gmail.com>
2022-12-03 11:19:24 -0600

    Esqueleto de applicación con estructura de componentes

---
+++ todo-app/src/components/FormTodo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormTodo = () => {
+     return (
+        <div>
+            Form Todo
+        </div>
+    );
+}
+
+export default FormTodo;
---
+++ todo-app/src/components/TaskList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Checkbox from "./Checkbox";
+
+const TaskList = () => {
+    return (
+        <div>
+            TaskList
+            <Checkbox/>
+            <Checkbox/>
+            <Checkbox/>
+        </div>
+    );
+}
+
+export default TaskList;
